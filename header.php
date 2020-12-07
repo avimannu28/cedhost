131113d830be7537ff9d93c5adb13a387472f479
avimannu28 <avimannu28@gmail.com>
2020-12-07 20:15:48 +0530

    initial

---
+++ header.php
@@ -0,0 +1,56 @@
+	<div class="header">
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<i class="sr-only">Toggle navigation</i>
+								<i class="icon-bar"></i>
+								<i class="icon-bar"></i>
+								<i class="icon-bar"></i>
+							</button>				  
+							<div class="navbar-brand">
+								<h1><a href="index.php">Planet Hosting</a></h1>
+							</div>
+						</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a href="index.php">Home <i class="sr-only">(current)</i></a></li>
+								<li class="active"><a href="about.php">About</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Service<i class="caret"></i></a>
+										<ul class="dropdown-menu">
+												
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
+									<ul class="dropdown-menu">
+										<li><a href="linuxhosting.php">Linux hosting</a></li>
+										<li><a href="wordpresshosting.php">WordPress Hosting</a></li>
+										<li><a href="windowshosting.php">Windows Hosting</a></li>
+										<li><a href="cmshosting.php">CMS Hosting</a></li>
+									</ul>			
+								</li>
+										</ul>
+									</li>
+								
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
+									<ul class="dropdown-menu">
+										<li><a href="linuxhosting.php">Linux hosting</a></li>
+										<li><a href="wordpresshosting.php">WordPress Hosting</a></li>
+										<li><a href="windowshosting.php">Windows Hosting</a></li>
+										<li><a href="cmshosting.php">CMS Hosting</a></li>
+									</ul>			
+								</li>
+								<li><a href="codes.php">Codes</a></li>
+								<li><a href="contact.php">Contact</a></li>
+							</ul>
+									  
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</div>
+		</div>
