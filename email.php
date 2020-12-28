d13b4fe54a1d1ffbffd4f8ad8e15fe2fe73a7a2c
avimannu28 <52011127+avimannu28@users.noreply.github.com>
2020-12-28 10:12:48 +0530

    Update email.php

--- email.php
+++ email.php
@@ -5,7 +5,7 @@
  *
  * @category Components
  * @package Packagename
- * @author Sumit kumar Pandey <avimannu28@gmail.com>
+ * @author Avinash Sharma <avimannu28@gmail.com>
  * @license http://www.php.net/license/3_01.txt PHP License 3.01
  * @link http://localhost/training/php%20mysql%20task1/register/signup.php
  */
